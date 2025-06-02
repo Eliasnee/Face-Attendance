993025feaa87f65bb278eb9f8703f47c26482cc2
Eliasnee <eliasneemtallah04@gmail.com>
2025-06-02 09:59:39 +0300

    Update generate_encodings_for_all_the_dataset.py

--- data_preprocessing/generate_encodings_for_all_the_dataset.py
+++ data_preprocessing/generate_encodings_for_all_the_dataset.py
@@ -1,13 +1,22 @@
 import face_recognition
 import os
 import pickle
+from tqdm import tqdm
+
 
 known_encodings = []
 known_names = []
+persons = os.listdir(r"known_faces_augmented")
 
-for person in os.listdir("known_faces_augmented"):
-    person_folder = os.path.join("known_faces", person)
-    for img_file in os.listdir(person_folder):
+# Loop through each person with tqdm
+for person in tqdm(persons, desc="Processing persons"):
+    person_folder = os.path.join("known_faces_augmented", person)
+
+
+    img_files = os.listdir(person_folder)
+    
+    # Loop through each image file with tqdm
+    for img_file in tqdm(img_files, desc=f"Encoding {person}", leave=False):
         image_path = os.path.join(person_folder, img_file)
         image = face_recognition.load_image_file(image_path)
         encs = face_recognition.face_encodings(image)
